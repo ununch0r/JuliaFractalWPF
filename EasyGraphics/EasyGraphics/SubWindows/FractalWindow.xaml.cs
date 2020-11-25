d2287081f7bacd522f7735082ed28720bdfffcd8
ununch0r <bodiastel123@gmail.com>
2020-11-25 20:07:31 +0200

    KG-1 Add fractals video

---
+++ EasyGraphics/EasyGraphics/SubWindows/FractalWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EasyGraphics.SubWindows
+{
+    /// <summary>
+    /// Interaction logic for FractalWindow.xaml
+    /// </summary>
+    public partial class FractalWindow : Window
+    {
+        public FractalWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
